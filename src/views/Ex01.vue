ae7c9f7cbea7b1f75f9f6d8dff8c1596e6bc7540
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-02-15 12:23:08 +0900

    Ex01.vueを作成し、ルーティングの設定を行った。

---
+++ src/views/Ex01.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>野球チーム一覧</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class XXXComponent extends Vue {}
+</script>
+
+<style scoped></style>
